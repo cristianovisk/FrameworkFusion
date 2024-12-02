c763d2df7ef5bab6a883e912039d79bc23b0d857
Cristiano Henrique dos Santos <cristianovisk@gmail.com>
2024-12-02 10:45:32 -0300

    add: informations about report

--- web.py
+++ web.py
@@ -6,7 +6,7 @@ from uuid import uuid4
 
 
 st.image(image="images/logo_frameworkfusion_ai.svg")
-
+st.echo("Test")
 bases = Map(primary="CWE", secundary="ASVS").bases
 primary = st.selectbox(label="Primary", options=bases)
 secundary = st.selectbox(label="Secundary", options=bases)
@@ -15,7 +15,9 @@ if st.button(label="Generate"):
     output = f"{uuid4()}.xlsx"
     try:
         map = Map(primary=primary, secundary=secundary).generate_table()
-        pd.DataFrame(map).to_excel(output, sheet_name="COMPARE", index=False)
+        df = pd.DataFrame(map)
+        df.to_excel(output, sheet_name="COMPARE", index=False)
+        st.success(f"Finded {len(df)} correlations between '{primary}' and '{secundary}'", icon="✅")
         with open(output, "rb") as file:
             btn = st.download_button(
                 label="Download Spreedsheet",
@@ -23,6 +25,7 @@ if st.button(label="Generate"):
                 file_name=output
             )
         st.balloons()
+        st.toast(f"**File generated**: *{output}* 📁")
         os.remove(output)
     except:
         st.error(f"Error to compare {primary} -> {secundary}")
